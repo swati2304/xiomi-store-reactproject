5f2c4fa9be0596190cc2d60dc1e782a2c2134bf4
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-10 22:06:46 +0530

    added routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Laptop from './view/tablets&Laptop/Laptop/laptop';
+import Home from "./views/Home/Home"
+import Contact from "./views/Contact/Contact"
+import Laptop from "./views/Laptop/Laptop"
+import Login from "./views/Login/Login"
+import Signup from "./views/Signup/Signup"
+import Smartphone from "./views/Smartphone/Smartphone"
+import Tv from "./views/Tv/Tv"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: "/laptop",
+      element: <Laptop />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/smartphone",
+        element: <Smartphone />,
+      },
+      {
+        path: "/tv",
+        element: <Tv />,
+      },
+  ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<>
+ <RouterProvider router={router}/>
+
 </>
 );
---
+++ src/views/Laptop/Laptop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Laptop() {
+  return (
+    <div><h1>Hello Guys </h1>
+    
+    </div>
+  )
+}
+
+export default Laptop
