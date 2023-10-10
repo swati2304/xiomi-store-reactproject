06cf74d5ba02c4d515d5bf7e9503c9238f4c3db1
swati2304 <swatinimje23@gmail.com>
2023-10-10 21:13:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Laptop from './view/tablets&Laptop/Laptop/laptop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(<>
+</>
 );
