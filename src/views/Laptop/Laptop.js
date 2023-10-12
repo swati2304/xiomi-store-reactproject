7a0b888d19a3875b5b820ca4103a9ca54f9cfb00
swati2304 <swatinimje23@gmail.com>
2023-10-12 22:06:47 +0530

    added carosoul

--- src/views/Laptop/Laptop.js
+++ src/views/Laptop/Laptop.js
@@ -1,25 +1,55 @@
 import React from 'react'
 import "./Laptop.css"
 import Card from '../../components/Card/Card';
-import ImgTablet from './../../views/Laptop/img/tab-5.jpeg'
+import ImgTablet from './../../views/Laptop/img/tab2.jpg'
 import Img from './../../views/Laptop/img/tab-2.jpeg'
 import Images from './../../views/Laptop/img/tab-3.jpeg'
 import Tablet from './../../views/Laptop/img/tab-4.jpeg'
-import Tab from './../../views/Laptop/img/tab-6.jpeg'
+import Tab from './../../views/Laptop/img/tab3.jpg'
 import ImgTab from './../../views/Laptop/img/tab-7.jpeg'
-import ImgTabs from './../../views/Laptop/img/tab-1.jpeg'
-import MainImg from './../../views/Laptop/img/8.jpeg'
+import ImgTabs from './../../views/Laptop/img/tab1.jpeg'
 import ImageOfLaptop from './../../views/Laptop/img/laptop-2.jpeg'
-
+import LapImage from './../../views/Laptop/img/lap.webp'
+import HomeImg1 from './../../views/Laptop/img/main-img-1.jpeg'
+import HomeImg2 from './../../views/Laptop/img/main-img-2.jpeg'
+import HomeImg3 from './../../views/Laptop/img/main-img-3.jpeg'
+import HomeImg4 from './../../views/Laptop/img/main-img-4.jpeg'
 
 
 function Laptop({img}) {
   return (
+    <>
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+
+<div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+    <img src={HomeImg1} class="d-block w-100"/>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+    <img src={HomeImg2}class="d-block w-100"/>
+    </div>
+    <div class="carousel-item">
+    <img src={HomeImg3} class="d-block w-100"/>
+    </div>
+    <div class="carousel-item">
+    <img src={HomeImg4} class="d-block w-100"/>
+    </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+</button>
+
+</div>
+
     <div>
-      <img src={MainImg} className='main-img'/>
       <h1 className='main-title'>Tablets </h1>
       <div className='container'>
-      <Card img={ImgTablet} price={9999} name="xiomi Mi pad post Gallery"/>
+      <Card img={ImgTablet} price={9999} name="xiomi Mi Pad 5"/>
       <Card img={Img} price={15999} name="xiomi Mi pad 6"/>
       <Card img={Images} price={22999} name="xiomi Mi pad 5 Review"/>
       <Card img={Tablet} price={8599} name="xiomi Mi pad 6 pro 11"/>
@@ -35,7 +65,7 @@ function Laptop({img}) {
       <h1 className='main-title'>Laptops </h1>
       <div className='container'>
       <Card img={ImageOfLaptop} price={75000} name="xiomi NoteBook pro 11"/>
-      <Card img={Img} price={15999} name="xiomi Mi pad 6"/>
+      <Card img={LapImage} price={15999} name="xiomi Mi pad 6"/>
       <Card img={Images} price={22999} name="xiomi Mi pad 5 Review"/>
       <Card img={Tablet} price={8599} name="xiomi Mi pad 6 pro 11"/>
       </div>
@@ -43,6 +73,7 @@ function Laptop({img}) {
       <br/>
       <br/>
     </div>
+    </>  
   )
 }
 
