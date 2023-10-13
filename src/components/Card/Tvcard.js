0a4a203ba393be6be24c61d225688fa46e5933e9
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-13 21:36:35 +0530

    changes in tv page

--- src/components/Card/Tvcard.js
+++ src/components/Card/Tvcard.js
@@ -1,15 +1,16 @@
 import React, {useState} from "react";
 import "./Tvcard.css"
 
-function Card1({name, price, img}){
+function Card1({name, price, img,del}){
   return(
     <div className="card-container">
     
       <h3 className="name-text">{name}</h3>
-      <p className="price-text">{price}</p>
+      <p className="price-text">₹{price}<del className="delprice"> ₹ {del}</del></p>
       <button className="btn-1">Buy Now</button>
       <button className="btn-1 btn-2">Learn More</button>
       <img src={img} className="tv-images"/>
+      <p className="symbolicbtn">RR</p>
     </div>
   )
 }
