04392d6a2a492b2a8114e8164cc65c859569c2dc
RutujaSEComp <rutujaamale396@gamil.com>
2023-10-13 23:08:00 +0530

    signin and signup pages added

--- src/views/Signup/Signup.js
+++ src/views/Signup/Signup.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Signup.css"
+export default Signup
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [forgotPassword, setforgotPassword] = useState('');
 
-export default Signup
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${email}\nPassword: ${password}\nConfirm Password: ${confirmPassword}\nforgotpassword: ${forgotPassword}` );
+  };
+  
+  
+
+  return (
+    <div className='signup-container'>
+      <p className='title'>Signup</p>
+
+    <form  onSubmit={handleSubmit}>
+      
+     <input  placeholder= 'email' type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    
+      <br/>
+     <input  placeholder='password' type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      <br/>
+     <input  placeholder="confirm-password"     type="password" id="confirm-password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+     <input  placeholder="forgot-password"     type="password" id="forgot-password" value={forgotPassword} onChange={(e) => setforgotPassword(e.target.value)} required />
+
+     <button type="submit" className='button'>Sign Up</button>
+    </form>
+    </div>
+  );
+}
