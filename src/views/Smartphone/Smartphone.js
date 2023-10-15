1113c0e1b7a89b7d920192f60e229d8433294f5d
gita321 <gitanjalikokate921@gmail.com>
2023-10-14 12:40:35 -1200

    added smartphone

--- src/views/Smartphone/Smartphone.js
+++ src/views/Smartphone/Smartphone.js
@@ -12,6 +12,17 @@ import Image2 from "./img/image2.webp"
 import Image5 from "./img/image8.jpg"
 import Image6 from "./img/image6.jpg"
 import Image7 from "./img/image7.jpg"
+import Image8 from "./img/m2.webp"
+import Image9 from "./img/m1.webp"
+import Image10 from "./img/m3.webp"
+import Image11 from "./img/m44.webp"
+import Image12 from "./img/b1.png"
+import Image13 from "./img/b2.jpg"
+import Image14 from "./img/b3.png"
+import Image15 from "./img/b4.png"
+import Image16 from "./img/cardimage.jpg"
+import Image17 from "./img/a2.png"
+
 
 
 
@@ -51,7 +62,7 @@ function Smartphone() {
           </button>
         </div>
       </div>
-      <p className='main-title'>MI Phones</p>
+      <p className='main-title'>MI Phones_📱</p>
       <div className='container'>
       <Phone img={ImgTablet} price={14000} name="Redmi Note 12 5G"/>
       <Phone img={Img} price={20999} name="Redmi 12 5G "/>
@@ -66,24 +77,86 @@ function Smartphone() {
       <Phone img={Image7} price={16999} name="Mi 10"/>
       </div>
       <br/>
-      <h4 className=' main-title-2'>Redmi series</h4>
+      <h3 className=' main-title-2'>Redmi Series..❤</h3>
 
       <div className='second-card-container'>
         <div className='first-card'>
+        <img  src={Image8}className='img-1'/>
           <h4 className='title-txt-1'>Redmi 12</h4>
           <p className='txt-1'>Octa Core Processor | 200mp + OIS</p>
           <p className='txt-1'>From ₹14,499</p>
-          <img className='img-1'></img>
-        </div>
+          <button className='btn-22'> Buy Now</button>
+          </div>
         <div className='second-card'>
+        <img src={Image9} className='img-1'/>
           <h4 className='title-txt-1'>Redmi A2 +</h4>
           <p className='txt-1'>Octa Core Processor | Up to 7GB Ram..</p>
           <p className='txt-1'>From ₹15,499</p>
-          <img src={Image7} className='img-2'/>
+          <button className='btn-22'> Buy Now</button>
+        </div>
+        </div>
+        <div className='second-card-container'>
+        <div className='first-card'>
+        <img  src={Image11}className='img-1'/>
+          <h4 className='title-txt-1'>Redmi Note 12</h4>
+          <p className='txt-1'>Super Note. Super Design</p>
+          <p className='txt-1'>From ₹11,999</p>
+          <button className='btn-22'> Buy Now</button>
+        </div>
+        <div className='second-card'>
+        <img src={Image10} className='img-1'/>
+          <h4 className='title-txt-1'>Redmi Note 12 Pro+ 5G</h4>
+          <p className='txt-1'>200MP + OIS</p>
+          <p className='txt-1'>From ₹27,999</p>
+          <button className='btn-22'> Buy Now</button>
+         </div>
+         </div>
+       <h3 className='main-title-3'>Our Services..⭐</h3>
+       <p className='main-title-33'>Trusted. Secure. Reliable</p>
+      <div className='third-card-container'>
+        <div className='third-card'>
+          <h4 className='main-title-3'>Mi Screen Protect</h4>
+           <p className='main-title-33'> Screen Protection against Accidental, Liquid Damage<br/>
+               Avail 2 Claims Per year</p>
+               <img  src={Image12}className='img-11'/>
+
+        </div>
+        <div className='third-card'>
+          <h4 className='main-title-3'>Mi Complete Protect</h4>
+          <p className='main-title-33'>Safeguard your smartphone Against Accidental & <br/>Liquid Damages</p>
+          <img  src={Image13}className='img-11'/>
         </div>
       </div>
+      <div className='third-card-container'>
+        <div className='third-card'>
+          <h4 className='main-title-3'>Smartphone Quality </h4>
+          <p className='main-title-33'>We are committed to provide the best quality at an <br/>honest price.</p>
+          <img  src={Image14}className='img-11'/>
+        </div>
+        <div className='third-card'>
+          <h4 className='main-title-3'> Mi Exchange</h4>
+          <p className='main-title-33'>Exchange your old Smart Phone in three simple steps</p>
+          <img  src={Image15}className='img-11'/>
+        </div>
+      </div>
+      <h3 className='main-title-3'> Redmi A2 Plus..⚜</h3>
+      <div className='last-container'>
+      <img src={Image16} className='last-img'/>
+      <h3 className='last-text'>The Redmi A2+ is designed to perform all
+          the essential tasks for you to stay connected, and productive..!</h3>
+      </div>
+      <div className='last-container'>
+       
+      <h3 className='last-text'>Redmi A2+ comes with a fingerprint sensor to securely unlock your smartphone</h3>
+      <img src={Image17} className='last-img'/>
+      
+      </div>
+      
+      
 
     </div>
+     
+
   )
 }
 
